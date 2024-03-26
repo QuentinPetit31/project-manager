720be6cf0ffbda5a3e3671c9274febccc8299bab
Quentin <you@example.com>
2024-03-26 09:10:33 +0100

    add login form

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+interface Login {
+  mail: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl<string>('', Validators.required),
+    password: new FormControl<string>('', Validators.required),
+  });
 
+  login() {
+    const formValue = this.form.getRawValue();
+    console.log('login = ', formValue);
+  }
 }
