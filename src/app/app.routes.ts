081d362f2684897b35a1e7e1ca8e4a0f93dde64f
Quentin <you@example.com>
2024-03-28 15:24:18 +0100

    add component project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { ExerciceComponent } from './page/exercice/exercice.component';
 import { LoginComponent } from './page/login/login.component';
 import { AuthGuard } from './guard/auth.guard';
 import { EnrollComponent } from './page/enroll/enroll.component';
+import { ProjectComponent } from './page/project/project.component';
 
 export const routes: Routes = [
+  { path: 'project', component: ProjectComponent, canActivate: [AuthGuard] },
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'enroll', component: EnrollComponent },
