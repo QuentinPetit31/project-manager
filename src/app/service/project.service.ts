85229ef69855a267273cbee3b268d7537b2f92e3
Quentin <you@example.com>
2024-04-05 12:04:13 +0200

    add design to detail-project

--- src/app/service/project.service.ts
+++ src/app/service/project.service.ts
@@ -14,25 +14,36 @@ export class ProjectService {
       personnes: ['toto', 'tata', 'titi'],
     },
     {
-      name: 'tito',
-      description: 'description',
-      startDate: '25/08/2000',
-      endDate: '27/08/2000',
-      personnes: ['toto', 'tata', 'titi'],
+      name: 'Basic Computer Skills Workshop',
+      description:
+        'Host a series of workshops to teach basic computer skills such as using email, browsing the internet, and using word processing software. Sarah will lead the workshops, with support from David and Emily.',
+      startDate: '25/02/2024',
+      endDate: '27/02/2024',
+      personnes: ['Sarah', 'David', 'Emily'],
     },
     {
-      name: 'totti',
-      description: 'description',
-      startDate: '25/08/2000',
-      endDate: '27/08/2000',
-      personnes: ['toto', 'tata', 'titi'],
+      name: 'Tech Support for Seniors',
+      description:
+        'Provide personalized tech support sessions for seniors to help them navigate their computers, smartphones, and tablets. James will coordinate the sessions, while Lisa and Michael will assist the seniors.',
+      startDate: '15/01/2011',
+      endDate: '17/01/2011',
+      personnes: ['James', 'Lisa', 'Michael'],
     },
     {
-      name: 'tata',
-      description: 'description',
-      startDate: '25/08/2000',
-      endDate: '27/08/2000',
-      personnes: ['toto', 'tata', 'titi'],
+      name: 'Coding Club for Kids',
+      description:
+        'Start a coding club for kids to learn programming basics in a fun and interactive way. Emma will lead the club, with help from Ethan and Olivia.',
+      startDate: '25/08/2020',
+      endDate: '27/08/2020',
+      personnes: ['Michael', 'David', 'Emma'],
+    },
+    {
+      name: 'Website Building Workshop',
+      description:
+        'Conduct a workshop to teach participants how to build their own websites using user-friendly platforms. Thomas will lead the workshop, with support from Rachel and Benjamin.',
+      startDate: '02/09/2023',
+      endDate: '29/08/2023',
+      personnes: ['Thomas', 'David', 'Emma', 'Quentin'],
     },
   ];
 
